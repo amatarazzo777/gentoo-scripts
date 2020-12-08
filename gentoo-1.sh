85510dea611a29efda8ba7fdffd061fcb5789509
Anthony Matarazzo <amatarazzo777@gmail.com>
2020-12-08 01:14:59 -0700

    Update gentoo-1.sh

--- gentoo-1.sh
+++ gentoo-1.sh
@@ -30,6 +30,7 @@ wget https://raw.githubusercontent.com/amatarazzo777/gentoo-scripts/main/make.co
 cp make.conf /mnt/gentoo/etc/portage/make.conf
 mirrorselect -i -o >> /mnt/gentoo/etc/portage/make.conf
 mkdir --parents /mnt/gentoo/etc/portage/repos.conf
+exit 0
 cp /mnt/gentoo/usr/share/portage/config/repos.conf /mnt/gentoo/etc/portage/repos.conf/gentoo.conf
 cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
 mount --types proc /proc /mnt/gentoo/proc
