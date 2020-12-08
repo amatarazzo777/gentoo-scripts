8c192620f91f7ff60c5037ebeb313e705f6c739c
Anthony Matarazzo <amatarazzo777@gmail.com>
2020-12-08 14:17:20 -0700

    Update gentoo-1.sh

--- gentoo-1.sh
+++ gentoo-1.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+#create partitions, the sizes are from the example gentoo
 parted -s -a optimal /dev/sda \
   mklabel gpt \
   unit mib \
@@ -13,55 +15,43 @@ parted -s -a optimal /dev/sda \
   name 4 rootfs  \
   set 2 boot on
 
+#the file systems are set for UEFI therefore 3 has to be fat32
 mkfs.fat -F 32 /dev/sda2
 mkfs.ext4 -F -F /dev/sda4
 mkswap /dev/sda3
 swapon /dev/sda3
 parted -s /dev/sda unit mib print
 
+#mount file system of storafe partition, ext4 and download stage 3
 mount /dev/sda4 /mnt/gentoo
 date
 cd /mnt/gentoo
 wget https://bouncer.gentoo.org/fetch/root/all/releases/amd64/autobuilds/20201206T214503Z/stage3-amd64-20201206T214503Z.tar.xz
 openssl dgst -r -sha512 sstage3-amd64-20201206T214503Z.tar.xz
 tar xpvf stage3-*.tar.xz --xattrs-include='*.*' --numeric-owner
+
+#download configuration make files, post chroot startup and kernel build settings
+wget https://raw.githubusercontent.com/amatarazzo777/gentoo-scripts/main/make.conf
 wget https://raw.githubusercontent.com/amatarazzo777/gentoo-scripts/main/make.conf
 
+# place settings into appropiate positions within the build tree
 cp make.conf /mnt/gentoo/etc/portage/make.conf
 mirrorselect -i -o >> /mnt/gentoo/etc/portage/make.conf
 mkdir --parents /mnt/gentoo/etc/portage/repos.conf
 cp /mnt/gentoo/usr/share/portage/config/repos.conf /mnt/gentoo/etc/portage/repos.conf/gentoo.conf
 cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
 
+#mount system folders
 mount --types proc /proc /mnt/gentoo/proc
 mount --rbind /sys /mnt/gentoo/sys
 mount --make-rslave /mnt/gentoo/sys
 mount --rbind /dev /mnt/gentoo/dev
 mount --make-rslave /mnt/gentoo/dev
+
+#test shared memory usage as a drive, needed
 test -L /dev/shm && rm /dev/shm && mkdir /dev/shm
 mount --types tmpfs --options nosuid,nodev,noexec shm /dev/shm
 chmod 1777 /dev/shm
+
+#kick off next part of building and kernel selections
 chroot /mnt/gentoo /bin/bash
-
-source /etc/profile
-export PS1="(chroot) ${PS1}"
-mount /dev/sda2 /boot
-emerge-webrsync
-emerge --sync
-eselect profile list
-emerge --ask --verbose --update --deep -newuse @world
-echo "license software type --> @FREE defaulted
-portageq envvar ACCEPT_LICENSE
-echo "US/Arizona" > /etc/timezone
-emerge --config sys-libs/timezone-data
-echo 'en_US ISO-8859-1\n en_US.UTF-8 UTF-8\n' >> /etc/locale.gen
-locale-gen
-eselect local list
-eselect local set 2
-env-update && source /etc/profile && export PS1="(chroot) ${PS1}"
-emerge --ask sys-kernel/gentoo-sources
-ls -l /usr/src/linux
-cd /usr/src/linux
-make menuconfig
-
-
